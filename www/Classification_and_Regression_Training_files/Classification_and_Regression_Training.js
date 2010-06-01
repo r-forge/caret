375eed252c02bebb4273ab40385e957f1848a5ab
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-01 20:36:27 +0000

    more MBSW
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@115 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/Classification_and_Regression_Training_files/Classification_and_Regression_Training.js
+++ www/Classification_and_Regression_Training_files/Classification_and_Regression_Training.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20091012
+// Created by iWeb 3.0.1 local-build-20100601
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Classification_and_Regression_Training_files/Classification_and_Regression_TrainingMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
