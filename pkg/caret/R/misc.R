84b5b83dfdf803c92eafed756a52fd83f3d17cd7
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-16 20:24:45 +0000

    added RMSE and R2 functions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@254 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/caret/R/misc.R
+++ pkg/caret/R/misc.R
@@ -386,6 +386,20 @@ decoerce <- function(x, grid, dot = FALSE)
 }  
 
 
+
+
+R2 <- function(pred, obs, formula = "corr", na.rm = FALSE)
+  {
+    n <- sum(complete.cases(pred))
+    switch(formula,
+           corr = cor(obs, pred, use = ifelse(na.rm, "complete.obs", "everything"))^2,
+           traditional = 1 - (sum((obs-pred)^2, na.rm = na.rm)/((n-1)*var(obs, na.rm = na.rm))))
+  }
+
+
+RMSE <- function(pred, obs, na.rm = FALSE) sqrt(mean((pred - obs)^2, na.rm = na.rm))
+
+
 defaultSummary <- function(data, lev = NULL, model = NULL)
   {
     if(is.character(data$obs)) data$obs <- factor(data$obs, levels = lev)
