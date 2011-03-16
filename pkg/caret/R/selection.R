df7511b761cd667991dd1f97c20ffef2bdc1d0cd
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-16 20:16:38 +0000

    added entries for Boruta, gcvEarth, qrnn and svmRadialCost
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@243 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/caret/R/selection.R
+++ pkg/caret/R/selection.R
@@ -25,7 +25,7 @@ byComplexity <- function(x, model)
            },
            rf =, rfNWS =, rfLSF =, parRF =, gpls =, pcr =, pls =, PLS =, pam =, cforest =,
            nb =, rpart =, ctree2 =, logitBoost=, J48 =, LMT =, ppr =, mda =, pda =, pda2 =,
-           lars =, lars2 =, Linda =, QdaCov =, icr =, qrf =
+           lars =, lars2 =, Linda =, QdaCov =, icr =, qrf =,Boruta =
            {
              x[order(x[,1]),]
            },
@@ -39,6 +39,10 @@ byComplexity <- function(x, model)
              # adapt. Also, if C is fixed, smaller values of sigma yeild more
              # complex boundaries
              x[order(x$C, -x$sigma),]
+           },
+           svmRadialCost =
+           {
+             x[order(x$C),]
            },
            svmpoly =, svmPoly =
            {
@@ -60,6 +64,10 @@ byComplexity <- function(x, model)
            {
              x[order(x$size, -x$decay),]
            },
+           qrnn =
+           {
+             x[order(x$n.hidden, -x$penalty),]
+           },           
            knn =, multinom =
            {
              x[order(-x[,1]),]
@@ -72,6 +80,10 @@ byComplexity <- function(x, model)
            {
              x[order(x$degree, x$nprune),]
            },
+           gcvEarth =
+           {
+             x[order(x$degree),]
+           },           
            treebag =, lda =, lm =, sddaLDA =, sddaQDA =,
            glmStepAIC =,lmStepAIC =, slda =, glm =, qda =, OneR =,
            rvmLinear =, lssvmLinear =, gaussprLinear =,
@@ -149,6 +161,10 @@ byComplexity <- function(x, model)
            hdda =
            {
              x[order(-x$threshold),]
+           },
+           plsGlmBinomial =, plsGlmGaussian =, plsGlmGamma =, plsGlmPoisson =
+           {
+             x[order(x$nt),]
            },
            logreg = x[order(x$ntrees, x$treesize),],
            logicBag = x[order(x$ntrees, x$nleaves),],
