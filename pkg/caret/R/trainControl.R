e35d9d93717599fe7d4ea20247ef7e3b1f67fd95
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-17 14:12:06 +0000

    added support for saving predictions and custom models
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@318 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/caret/R/trainControl.R
+++ pkg/caret/R/trainControl.R
@@ -4,10 +4,12 @@ trainControl <- function(method = "boot",
                          verboseIter = FALSE,
                          returnData = TRUE,
                          returnResamp = "final",
+                         savePredictions = FALSE,
                          p = .75,
                          classProbs = FALSE,
                          summaryFunction = defaultSummary,
                          selectionFunction = "best",
+                         custom = NULL,
                          preProcOptions = list(thresh = 0.95, ICAcomp = 3, k = 5),
                          index = NULL,
                          timingSamps = 0,
@@ -18,18 +20,31 @@ trainControl <- function(method = "boot",
   if(length(predictionBounds) > 0 && length(predictionBounds) != 2) stop("'predictionBounds' should be a logical or numeric vector of length 2")
   if(any(names(preProcOptions) == "method")) stop("'method' cannot be specified here")
   if(any(names(preProcOptions) == "x")) stop("'x' cannot be specified here")
-  
+
+  if(!is.null(custom))
+    {
+      cNames <- names(custom)
+      reqNames <- c("parameters", "model", "prediction", "probability", "sort")
+      if(!all(reqNames %in% cNames))
+        stop(paste("The custom argument must be a list with elements", paste(reqNames, collapse = ", ")))
+      if(!is.function(custom$model)) stop("The 'model' element should be a function")
+      if(!is.function(custom$prediction)) stop("The 'prediction' element should be a function")
+      if(!is.function(custom$probability) & !is.null(custom$probability)) stop("The 'probability' element should be a function or NULL")
+      if(!is.function(custom$sort)) stop("The 'sort' element should be a function")
+    }
   list(method = method,
        number = number,
        repeats = repeats,
        verboseIter = verboseIter,
        returnData = returnData,
        returnResamp = returnResamp,
+       savePredictions = savePredictions,
        p = p,
        classProbs = classProbs,
        summaryFunction = summaryFunction,
        selectionFunction = selectionFunction,
        preProcOptions = preProcOptions,
+       custom = custom,
        index = index,
        timingSamps = timingSamps,
        predictionBounds = predictionBounds)
