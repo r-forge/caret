bf32797473fb1a8ada308b53ba72318069e8fd61
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-11 19:31:49 +0000

    backup of old website
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@433 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/Archive/Classification_and_Regression_Training_files/Classification_and_Regression_Training.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20120531
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Classification_and_Regression_Training_files/Classification_and_Regression_TrainingMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
