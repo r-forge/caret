90eaf70ea55fd536dee8f1c079a8f2c96f097f32
kuhn (Dr. ) <mxkuhn@gmail.com>
2013-09-10 16:23:37 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@576 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/caret/inst/models/glmnet.R
@@ -0,0 +1,121 @@
+modelInfo <- list(library = "glmnet",
+                  type = c("Regression", "Classification"),
+                  parameters = data.frame(parameter = c('alpha', 'lambda'),
+                                          class = c("numeric", "numeric"),
+                                          label = c('Mixing Percentage', 'Regularization Parameter')),
+                  grid = function(x, y, len = NULL) 
+                    expand.grid(.alpha = seq(0.1, 1, length = len),
+                                .lambda = seq(.1, 3, length = 3 * len)),
+                  loop = function(grid) {   
+                    uniqueAlpha <- unique(grid$.alpha)
+                    loop <- data.frame(.alpha = uniqueAlpha)
+                    loop$.lambda <- NA
+                    submodels <- vector(mode = "list", length = length(uniqueAlpha))
+                    
+                    for(i in seq(along = uniqueAlpha))
+                      submodels[[i]] <- data.frame(.lambda = subset(grid, 
+                                                                    subset = .alpha == uniqueAlpha[i])$.lambda)
+                    list(loop = loop, submodels = submodels)
+                  },
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    numLev <- if(is.character(y) | is.factor(y)) length(levels(y)) else NA
+                    
+                    theDots <- list(...)
+                    
+                    if(all(names(theDots) != "family"))
+                    {
+                      if(!is.na(numLev))
+                      {
+                        fam <- ifelse(numLev > 2, "multinomial", "binomial")
+                      } else fam <- "gaussian"
+                      
+                      theDots$family <- fam   
+                    }
+                    
+                    ## pass in any model weights
+                    if(!is.null(wts)) theDots$weights <- wts
+                    
+                    modelArgs <- c(list(x = as.matrix(x),
+                                        y = y,
+                                        alpha = param$.alpha),
+                                   theDots)
+                    
+                    out <- do.call("glmnet", modelArgs) 
+                    out 
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    if(!is.matrix(newdata)) newdata <- as.matrix(newdata)
+                    
+                    if(!is.null(submodels))
+                    {
+                      if(length(modelFit$obsLevels) < 2)
+                      {
+                        out <- as.list(as.data.frame(predict(modelFit, newdata, s = submodels$.lambda)))
+                      } else {
+                        out <- predict(modelFit, newdata, s = submodels$.lambda, type = "class")
+                        out <- as.list(as.data.frame(out, stringsAsFactors = FALSE))
+                      }
+                    } else {
+                      
+                      if(is.null(modelFit$lambdaOpt))
+                        stop("optimal lambda not saved by train; needs a single lambda value")
+                      if(length(modelFit$obsLevels) < 2)
+                      {
+                        out <- predict(modelFit, newdata, s = modelFit$lambdaOpt)[,1]
+                      } else {
+                        out <- predict(modelFit, newdata, s = modelFit$lambdaOpt, type = "class")[,1]
+                      }
+                    }
+                    out       
+                  },
+                  prob = function(modelFit, newdata, submodels = NULL) {
+                    obsLevels <- if("classnames" %in% names(modelFit)) modelFit$classnames else NULL
+                    if(length(obsLevels) == 2)
+                    {
+                      if(!is.null(submodels))
+                      {
+                        probs <- predict(modelFit,
+                                         as.matrix(newdata),
+                                         s = submodels$.lambda,
+                                         type = "response")
+                        
+                        probs <- as.list(as.data.frame(probs))
+                        probs <- lapply(probs,
+                                        function(x, lev)
+                                        {
+                                          tmp <- data.frame(x, 1-x)
+                                          names(tmp) <- lev
+                                          tmp
+                                        },
+                                        lev = modelFit$obsLevels)
+                        
+                      } else {
+                        probs <- predict(modelFit,
+                                         as.matrix(newdata),
+                                         s = modelFit$lambdaOpt,
+                                         type = "response")
+                        probs <- cbind(1-probs, probs)
+                        colnames(probs) <- modelFit$obsLevels
+                      }
+                    } else {
+                      if(!is.null(submodels))
+                      {
+                        ## This generates a 3d array
+                        probs <- predict(modelFit,
+                                         as.matrix(newdata),
+                                         s = submodels$.lambda,
+                                         type = "response")
+                        ## convert it to a list of 2d structures
+                        probs <- apply(probs, 3, function(x) data.frame(x))
+                      } else {
+                        probs <- predict(modelFit,
+                                         as.matrix(newdata),
+                                         s = modelFit$lambdaOpt,
+                                         type = "response")
+                        probs <- probs[,,1]
+                      }
+                    }
+                    
+                    probs
+                  },
+                  sort = function(x) x)
---
+++ V6/caret/inst/models/lars.R
@@ -0,0 +1,41 @@
+modelInfo <- list(library = "lars",
+                  type = "Regression",
+                  parameters = data.frame(parameter = 'fraction',
+                                          class = "numeric",
+                                          label = 'Fraction'),
+                  grid = function(x, y, len = NULL)
+                    expand.grid(.fraction = seq(0.05, 1, length = len)),
+                  loop = function(grid) {   
+                    grid <- grid[order(grid$.fraction, decreasing = TRUE),, drop = FALSE]
+                    loop <- grid[1,,drop = FALSE]
+                    submodels <- list(grid[-1,,drop = FALSE])     
+                    list(loop = loop, submodels = submodels)
+                  },
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) 
+                    lars(as.matrix(x), y, ...),
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    out <- predict(modelFit,
+                                   as.matrix(newdata),
+                                   type = "fit",
+                                   mode = "fraction",
+                                   s = modelFit$tuneValue$.fraction)$fit
+                    
+                    if(!is.null(submodels))
+                    {
+                      tmp <- vector(mode = "list", length = nrow(submodels) + 1)
+                      tmp[[1]] <- out
+                      
+                      for(j in seq(along = submodels$.fraction))
+                      {
+                        tmp[[j+1]] <- predict(modelFit,
+                                              as.matrix(newdata),
+                                              type = "fit",
+                                              mode = "fraction",
+                                              s = submodels$.fraction[j])$fit
+                      }
+                      out <- tmp
+                    }
+                    out        
+                  },
+                  prob = NULL,
+                  sort = function(x) x)
