ab442c52b4fa6c263d077b2e945b84e7babaa572
kuhn (Dr. ) <mxkuhn@gmail.com>
2013-10-04 23:16:34 +0000

    inital version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@582 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/caret/inst/models/ada.R
@@ -0,0 +1,42 @@
+modelInfo <- list(library = "ada",
+                  loop = NULL,
+                  type = c("Classification"),
+                  parameters = data.frame(parameter = c('iter', 'maxdepth', 'nu'),
+                                          class = rep("numeric", 3),
+                                          label = c('#Trees', 'Max Tree Depth', 'Learning Rate')),
+                  grid = function(x, y, len = NULL) expand.grid(.iter = floor((1:len) * 50),
+                                                                .maxdepth = seq(1, len),         
+                                                                .nu = .1),
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    theDots <- list(...)
+                    
+                    if(any(names(theDots) == "control"))
+                    {
+                      theDots$control$maxdepth <- param$.maxdepth 
+                      ctl <- theDots$control
+                      theDots$control <- NULL
+                      
+                    } else ctl <- rpart.control(maxdepth = param$.maxdepth) 
+                    
+                    modelArgs <- c(list(x = x,
+                                        y = y,
+                                        iter = param$.iter,
+                                        nu = param$.nu,              
+                                        control = ctl),
+                                   theDots)
+                    
+                    out <- do.call("ada", modelArgs)
+                    
+#                     out$call["x"] <- "xData"         
+#                     out$call["y"] <- "yData"  
+                    
+                    out     
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL)
+                    predict(modelFit, newdata),
+                  prob = function(modelFit, newdata, submodels = NULL){
+                    out <- predict(modelFit, newdata, type = "prob")
+                    colnames(out) <-  modelFit$obsLevels
+                    out
+                  },
+                  sort = function(x) x)
---
+++ V6/caret/inst/models/nnet.R
@@ -0,0 +1,47 @@
+modelInfo <- list(library = "nnet",
+                  loop = NULL,
+                  type = c("Classification", "Regression"),
+                  parameters = data.frame(parameter = c('size', 'decay'),
+                                          class = rep("numeric", 2),
+                                          label = c('#Hidden Units', 'Weight Decay')),
+                  grid = function(x, y, len = NULL) expand.grid(.size = ((1:len) * 2) - 1, 
+                                                                .decay = c(0, 10 ^ seq(-1, -4, length = len - 1))),
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    library(nnet)
+                    dat <- x
+                    dat$.outcome <- y
+                    if(!is.null(wts))
+                    {
+                      out <- nnet(.outcome ~ .,
+                                  data = dat,
+                                  weights = wts,                                       
+                                  size = param$.size,
+                                  decay = param$.decay,
+                                  ...)
+                    } else out <- nnet(.outcome ~ .,
+                                       data = dat,
+                                       size = param$.size,
+                                       decay = param$.decay,
+                                       ...)
+                    out
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL)
+                  {
+                    if(modelFit$problemType == "Classification")
+                    {
+                      out <- predict(modelFit, newdata, type="class")
+                    } else {
+                      out  <- predict(modelFit, newdata, type="raw")
+                    }
+                    out
+                  },
+                  prob = function(modelFit, newdata, submodels = NULL){
+                    out <- predict(modelFit, newdata)
+                    if(ncol(as.data.frame(out)) == 1)
+                    {
+                      out <- cbind(out, 1-out)
+                      dimnames(out)[[2]] <-  rev(modelFit$obsLevels)
+                    }
+                    out
+                  },
+                  sort = function(x) x)
