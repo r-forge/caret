c8713fa5677c309037c82b04cd7d76d19f93217c
weston (Steve Weston) <stephen.b.weston@gmail.com>
2013-10-26 21:35:05 +0000

    First attempt at ctree model.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@621 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/caret/inst/models/ctree.R
@@ -0,0 +1,53 @@
+modelInfo <- list(library = "party",
+                  loop = NULL,
+                  type = c("Classification", "Regression"),
+                  parameters = data.frame(parameter = 'mincriterion',
+                                          class = 'unknown',
+                                          label = '1 - P-Value Threshold'),
+                  grid = function(x, y, len = NULL) {
+                    data.frame(.mincriterion = seq(from = .99, to = 0.01, length = len))
+                  },
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    dat <- x
+                    dat$.outcome <- y
+
+                    theDots <- list(...)
+
+                    if(any(names(theDots) == "controls"))
+                      {
+                        theDots$controls@gtctrl@mincriterion <- param$.mincriterion
+                        ctl <- theDots$controls
+                        theDots$controls <- NULL
+
+                      } else ctl <- ctree_control(mincriterion = param$.mincriterion)
+
+                    ## pass in any model weights
+                    if(!is.null(wts)) theDots$weights <- wts
+
+                    modelArgs <- c(
+                                   list(
+                                        formula = as.formula(".outcome ~ ."),
+                                        data = dat,
+                                        controls = ctl),
+                                   theDots)
+
+                    out <- do.call(getFromNamespace("ctree", "party"), modelArgs)
+                    out
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    out <- predict(modelFit, newdata)
+                    if(!is.null(modelFit@responses@levels$.outcome)) out <- as.character(out)
+                    if(is.matrix(out)) out <- out[,1]
+                    out
+                  },
+                  prob = function(modelFit, newdata, submodels = NULL) {
+                    obsLevels <- levels(modelFit@data@get("response")[,1])
+                    rawProbs <- treeresponse(modelFit, newdata)
+                    probMatrix <- matrix(unlist(rawProbs), ncol = length(obsLevels), byrow = TRUE)
+                    out <- data.frame(probMatrix)
+                    colnames(out) <- obsLevels
+                    rownames(out) <- NULL
+                    out
+                  },
+                  tags = c('unknown'),
+                  sort = function(x) x)
