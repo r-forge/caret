a4c1d07308eb024c1c5ffc634c661338000d4217
weston (Steve Weston) <stephen.b.weston@gmail.com>
2013-10-26 20:25:01 +0000

    Removed hardcoded path for models directory.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@619 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- V6/caret/inst/parseModels.R
+++ V6/caret/inst/parseModels.R
@@ -1,11 +1,12 @@
-setwd("~/Code/caret/V6//caret/inst/models/")
-modelFiles <- list.files(pattern = "\\.R$")
+# setwd("~/Code/caret/V6//caret/inst/models/")
+modelFiles <- list.files(path = "models", pattern = "\\.R$")
 
 models <- vector(mode = "list", length = length(modelFiles))
 names(models) <- gsub("\\.R", "", modelFiles)
 
 for(i in seq(along = modelFiles)) {
-  source(modelFiles[i])
+  cat(sprintf("sourcing %s...\n", modelFiles[i]))
+  source(file.path("models", modelFiles[i]))
   models[[i]] <- modelInfo
   rm(modelInfo)
 }
