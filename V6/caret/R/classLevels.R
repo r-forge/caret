71b734f0566919f1c766b8ec1a5dd813f861bb1a
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-28 17:11:21 +0000

    changed generic from classLevels to extend levels
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@624 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- V6/caret/R/classLevels.R
+++ V6/caret/R/classLevels.R
@@ -1,113 +1,118 @@
-"classLevels" <-   function(x, ...) UseMethod("classLevels")
-
-classLevels.default <- function(x, ...) NULL
-
-classLevels.train <- function(x, ...) classLevels(x$finalModel)
-
-classLevels.ksvm <- function(x, ...) lev(x)
-classLevels.lssvm <- function(x, ...) lev(x)
-classLevels.gausspr <- function(x, ...) lev(x)
-
-classLevels.LdaClassic <- function(x, ...) names(x@prior)
-classLevels.QdaClassic <- function(x, ...) names(x@prior)
-
-classLevels.BinaryTree <- function(x, ...) levels(x@data@get("response")[,1])
-classLevels.RandomForest <- function(x, ...) levels(x@data@get("response")[,1])
-
-classLevels.lda <- function(x, ...) names(x$prior)
-classLevels.lda <- function(x, ...) names(x$prior)
-
-classLevels.rda <- function(x, ...) names(x$prior)
-
-classLevels.gbm <- function(x, ...) if(any(names(x) == "obsLevels")) x$obsLevels else NULL
-
-classLevels.randomForest <- function(x, ...) x$classes
-
-classLevels.nnet <- function(x, ...) x$coefnames
-
-classLevels.pcaNNet <- function(x, ...) x$model$coefnames
-
-classLevels.gpls <- function(x, ...) x$model$levs
-
-classLevels.rpart <- function(x, ...) attr(x, "ylevels")
-
-classLevels.plsda <- function(x, ...) x$obsLevels
-
-classLevels.pamrtrained <- function(x, ...) names(x$prior)
-
-classLevels.knn3 <- function(x, ...) levels(x$learn$y)
-
-classLevels.NaiveBayes <- function(x, ...) x$levels
-
-classLevels.earth <- function(x, ...) x$levels
-
-classLevels.fda <- function(x, ...) names(x$prior)
-
-classLevels.bagFDA <- function(x, ...) x$levels
-
-classLevels.classbag <- function(x, ...) levels(x$y)
-
-classLevels.glm <- function(x, ...) if(any(names(x) == "obsLevels")) x$obsLevels else NULL
-
-classLevels.glmboost <- function(x, ...) levels(x$response)
-classLevels.gamboost <- function(x, ...) levels(x$response)
-classLevels.blackboost <- function(x, ...) levels(x$response)
-
-classLevels.glmnet <- function(x, ...) if(any(names(x) == "obsLevels")) x$obsLevels else NULL
-
-classLevels.sdda <- function(x, ...) rownames(x$means)
-
-classLevels.LogitBoost <- function(x, ...) x$lablist
-
-classLevels.LogitBoost <- function(x, ...) x$lablist
-
-classLevels.Weka_classifier <- function(x, ...) x$levels
-
-classLevels.slda <- function(x, ...) names(x$mylda$prior)
-
-classLevels.sda <- function(x, ...)
+levels.train <- function(x) {
+  if(x$modelType == "Classification")
   {
-    ## objects from package sparseLDA and sda have the
-    ## same class name
-    if(any(names(x) == "regularization")) names(x$prior) else x$classes
-  }
-
-classLevels.splsda <- function(x, ...)
-  {
-    ## objects from package caret and spls have the
-    ## same class name
-
-    isSpls <- !is.null(x$class.fit)
-    if(isSpls) names(x$class.fit$prior) else x$obsLevels
-  }
+    if(!isS4(x)) {
+      ## add a try here and or look for null values
+      out <- x$finalModel$obsLevels
+    } else out <- levels(x$finalModel)
+  } else out <- NULL
+  out
+}
 
 
-classLevels.mda <- function(x, ...) unique(unlist(lapply(strsplit(rownames(x$means), ".", fixed = TRUE), function(x) x[1])))
+levels.ksvm <- function(x) {
+  library(kernlab)
+  lev(x)
+}
+levels.lssvm <- function(x) {
+  library(kernlab)
+  lev(x)
+}
+levels.gausspr <- function(x) {
+  library(kernlab)
+  lev(x)
+}
 
-classLevels.VBMP.obj <- function(x, ...) if(any(names(x) == "obsLevels")) x$obsLevels else NULL
+levels.LdaClassic <- function(x) names(x@prior)
+levels.QdaClassic <- function(x) names(x@prior)
 
-classLevels.nodeHarvest <- function(x, ...) unique(unlist(lapply(strsplit(rownames(x$means), ".", fixed = TRUE), function(x) x[1])))
+levels.BinaryTree <- function(x) levels(x@data@get("response")[,1])
+levels.RandomForest <- function(x) levels(x@data@get("response")[,1])
 
-classLevels.stepclass <- function(x, ...) classLevels(x$fit)
+levels.lda <- function(x) names(x$prior)
 
-classLevels.plr <- function(x, ...) unique(unlist(lapply(strsplit(rownames(x$means), ".", fixed = TRUE), function(x) x[1])))
+levels.rda <- function(x) names(x$prior)
+
+levels.gbm <- function(x) if(any(names(x) == "obsLevels")) x$obsLevels else NULL
+
+levels.randomForest <- function(x) x$classes
+
+levels.nnet <- function(x) x$coefnames
+
+levels.pcaNNet <- function(x) x$model$coefnames
+
+levels.gpls <- function(x) x$model$levs
+
+levels.rpart <- function(x) attr(x, "ylevels")
+
+levels.plsda <- function(x) x$obsLevels
+
+levels.pamrtrained <- function(x) names(x$prior)
+
+levels.knn3 <- function(x) levels(x$learn$y)
+
+levels.NaiveBayes <- function(x) x$levels
+
+levels.earth <- function(x) x$levels
+
+levels.fda <- function(x) names(x$prior)
+
+levels.bagFDA <- function(x) x$levels
+
+levels.classbag <- function(x) levels(x$y)
+
+levels.glm <- function(x) if(any(names(x) == "obsLevels")) x$obsLevels else NULL
+
+levels.glmboost <- function(x) levels(x$response)
+levels.gamboost <- function(x) levels(x$response)
+levels.blackboost <- function(x) levels(x$response)
+
+levels.glmnet <- function(x) if(any(names(x) == "obsLevels")) x$obsLevels else NULL
+
+levels.sdda <- function(x) rownames(x$means)
+
+levels.LogitBoost <- function(x) x$lablist
+
+levels.LogitBoost <- function(x) x$lablist
+
+levels.Weka_classifier <- function(x) x$levels
+
+levels.slda <- function(x) names(x$mylda$prior)
+
+levels.sda <- function(x)
+{
+  ## objects from package sparseLDA and sda have the
+  ## same class name
+  if(any(names(x) == "regularization")) names(x$prior) else x$classes
+}
+
+levels.splsda <- function(x)
+{
+  ## objects from package caret and spls have the
+  ## same class name
+  
+  isSpls <- !is.null(x$class.fit)
+  if(isSpls) names(x$class.fit$prior) else x$obsLevels
+}
 
 
+levels.mda <- function(x) unique(unlist(lapply(strsplit(rownames(x$means), ".", fixed = TRUE), function(x) x[1])))
 
-#  classLevels.rocc <- function(x, ...)
-#  classLevels.partDSA <- function(x, ...)
-#  classLevels.hda <- function(x, ...)
-#  classLevels.scrda <- function(x, ...)
-#  classLevels.plr <- function(x, ...) 
-#  classLevels.hdda <- function(x, ...) 
-#  classLevels.logreg <- function(x, ...) 
-#  classLevels.logforest <- function(x, ...) 
-#  classLevels.gam <- function(x, ...) 
-#  classLevels.gamLoess <- function(x, ...) 
-#  classLevels.gamSpline <- function(x, ...) 
-#  classLevels.Boruta <- function(x, ...) 
+levels.VBMP.obj <- function(x) if(any(names(x) == "obsLevels")) x$obsLevels else NULL
 
+levels.nodeHarvest <- function(x) unique(unlist(lapply(strsplit(rownames(x$means), ".", fixed = TRUE), function(x) x[1])))
 
+levels.stepclass <- function(x) levels(x$fit)
+
+levels.plr <- function(x) unique(unlist(lapply(strsplit(rownames(x$means), ".", fixed = TRUE), function(x) x[1])))
+
+if(FALSE)
+{
+  lvlList <- sort(ls(pattern = "levels\\."))
+  lvlList <- gsub("levels\\.", "", lvlList)
+  lvlList <- paste("S3method(levels, ", lvlList, ")", sep = "")
+  cat(paste(lvlList, collapse = "\n", sep = ""))
+}
 
 
 
