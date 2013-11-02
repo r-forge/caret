ecbdeb805f4fe0af221ab8d790c3fbe6e92c32f7
kuhn (Dr. ) <mxkuhn@gmail.com>
2013-11-02 15:44:37 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@652 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/caret/inst/models/CSimca.R
@@ -0,0 +1,16 @@
+modelInfo <- list(library = "rrcovHD",
+                  loop = NULL,
+                  type = c("Classification"),
+                  parameters = data.frame(parameter = 'parameter',
+                                          class = NA,
+                                          label = 'parameter'),
+                  grid = function(x, y, len = NULL) {
+                    data.frame(parameter = "none")
+                  },
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) 
+                    CSimca(x, y, ...),
+                  predict = function(modelFit, newdata, submodels = NULL) 
+                    predict(modelFit, newdata)@classification,
+                  prob = NULL,
+                  tags = c('Robust Methods'),
+                  sort = function(x) x)
