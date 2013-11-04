9d3f20596b78385205683a5bbce94f23027afc61
kuhn (Dr. ) <mxkuhn@gmail.com>
2013-11-04 15:37:44 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@655 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/caret/inst/models/bagFDA.R
@@ -0,0 +1,32 @@
+modelInfo <- list(library = c("earth", "mda"),
+                  loop = NULL,
+                  type = "Classification",
+                  parameters = data.frame(parameter = c("degree", "nprune"),
+                                          class = c("numeric", "numeric"),
+                                          label = c('Product Degree', '#Terms')),
+                  grid = function(x, y, len = NULL) {
+                    dat <- if(!is.data.frame(x)) as.data.frame(x) else x
+                    dat$.outcome <- y
+                    
+                    mod <- fda( .outcome~., data = dat, method = earth, pmethod = "none")
+                    maxTerms <- nrow(mod$fit$dirs) - 1
+                    
+                    maxTerms <- min(200, floor(maxTerms * .75) + 2)
+                    data.frame(.nprune = unique(floor(seq(2, to = maxTerms, length = len))),
+                               .degree = 1)
+                  },
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    dat <- x
+                    dat$.outcome <- y
+                    bagFDA(.outcome ~ ., 
+                           data = dat, 
+                           degree = param$.degree,
+                           nprune = param$.nprune, ...)
+                  },
+                  tags = c("Multivariate Adaptive Regression Splines", "Ensemble Model", 
+                           "Implicit Feature Selection", "Bagging"),
+                  predict = function(modelFit, newdata, submodels = NULL) 
+                    predict(modelFit , newdata),
+                  prob = function(modelFit, newdata, submodels = NULL) 
+                    predict(modelFit, newdata, type= "probs"),
+                  sort = function(x) x)
