d2f689abc759f15c9fa0336291ff5531753f3de3
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-18 01:06:54 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@660 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/caret/inst/models/sda.R
@@ -0,0 +1,20 @@
+modelInfo <- list(library = c("sda"),
+                  loop = NULL,
+                  type = c("Classification"),
+                  parameters = data.frame(parameter = c('diagonal','lambda'),
+                                          class = c("logical", "numeric"),
+                                          label = c('Diagonalize','shrinkage')),
+                  grid = function(x, y, len = NULL) 
+                    data.frame(.diagonal = FALSE, .lambda = seq(0, 1, length = len)),
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...)
+                    sda::sda(as.matrix(x), 
+                             y, 
+                             diagonal = param$.diagonal, 
+                             lambda = param$.lambda, 
+                             ...),
+                  predict = function(modelFit, newdata, submodels = NULL)
+                    sda::predict.sda(modelFit, as.matrix(newdata))$class,
+                  prob = function(modelFit, newdata, submodels = NULL)
+                    sda::predict.sda(modelFit, as.matrix(newdata))$posterior,
+                  tags = c("Discriminant Analysis", "Regularization"),
+                  sort = function(x) x)
