83c63558676186333b082aeb7e6bc4914a43aede
kuhn (Dr. ) <mxkuhn@gmail.com>
2013-11-27 11:57:18 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@677 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/RegressionTests/Code/bagEarth.R
@@ -0,0 +1,100 @@
+library(caret)
+timestamp <- format(Sys.time(), "%Y_%m_%d_%H_%M")
+
+model <- "bagEarth"
+tests <- c("test_class_cv_model", "test_class_pred", "test_class_prob",
+           "test_class_loo_model", "test_levels", "test_reg_cv_model",
+           "test_reg_loo_model", "test_reg_oob_model", "test_class_oob_model")
+
+#########################################################################
+
+set.seed(2)
+training <- twoClassSim(100)
+testing <- twoClassSim(500)
+trainX <- training[, -ncol(training)]
+trainY <- training$Class
+
+cctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all",
+                       classProbs = TRUE, 
+                       summaryFunction = twoClassSummary)
+cctrl2 <- trainControl(method = "LOOCV",
+                       classProbs = TRUE, summaryFunction = twoClassSummary)
+cctrl3 <- trainControl(method = "oob")
+
+set.seed(849)
+test_class_cv_model <- train(trainX, trainY, 
+                             method = "bagEarth", 
+                             trControl = cctrl1,
+                             metric = "ROC", 
+                             preProc = c("center", "scale"),
+                             B = 10)
+
+test_class_pred <- predict(test_class_cv_model, testing[, -ncol(testing)])
+test_class_prob <- predict(test_class_cv_model, testing[, -ncol(testing)], type = "prob")
+
+set.seed(849)
+test_class_loo_model <- train(trainX, trainY, 
+                              method = "bagEarth", 
+                              trControl = cctrl2,
+                              metric = "ROC", 
+                              preProc = c("center", "scale"),
+                              B = 10)
+test_levels <- levels(test_class_cv_model)
+
+set.seed(849)
+test_class_oob_model <- train(trainX, trainY, 
+                              method = "bagEarth", 
+                              trControl = cctrl3,
+                              B = 10)
+
+#########################################################################
+
+data(BloodBrain)
+bbbDescr <-bbbDescr[, -nearZeroVar(bbbDescr)]
+bbbDescr <-bbbDescr[, -findCorrelation(cor(bbbDescr), .5)]
+
+set.seed(2)
+
+inTrain <- createDataPartition(logBBB, p = .5)
+trainX <-bbbDescr[inTrain[[1]], ]
+trainY <- logBBB[inTrain[[1]]]
+testX <- bbbDescr[-inTrain[[1]], ]
+testY <- logBBB[-inTrain[[1]]]
+
+rctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all")
+rctrl2 <- trainControl(method = "LOOCV")
+rctrl3 <- trainControl(method = "oob")
+
+set.seed(849)
+test_reg_cv_model <- train(trainX, trainY, 
+                           method = "bagEarth", 
+                           trControl = rctrl1,
+                           preProc = c("center", "scale"),
+                           B = 10)
+test_reg_pred <- predict(test_reg_cv_model, testX)
+
+set.seed(849)
+test_reg_loo_model <- train(trainX, trainY, 
+                            method = "bagEarth",
+                            trControl = rctrl2,
+                            preProc = c("center", "scale"),
+                            B = 10)
+
+set.seed(849)
+test_reg_oob_model <- train(trainX, trainY, 
+                            method = "bagEarth",
+                            trControl = rctrl3,
+                            preProc = c("center", "scale"),
+                            B = 10)
+
+#########################################################################
+
+
+sInfo <- sessionInfo()
+
+save(list = c(tests, "sInfo", "timestamp"),
+     file = file.path(getwd(), paste(model, ".RData", sep = "")))
+
+q("no")
+
+
---
+++ V6/RegressionTests/Code/rFerns.R
@@ -0,0 +1,44 @@
+library(caret)
+timestamp <- format(Sys.time(), "%Y_%m_%d_%H_%M")
+
+model <- "rFerns"
+tests <- c("test_class_cv_model", "test_class_pred", 
+           "test_class_loo_model", "test_levels")
+
+#########################################################################
+
+set.seed(2)
+training <- twoClassSim(100)
+testing <- twoClassSim(500)
+trainX <- training[, -ncol(training)]
+trainY <- training$Class
+
+cctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all")
+cctrl2 <- trainControl(method = "LOOCV")
+
+set.seed(849)
+test_class_cv_model <- train(trainX, trainY, 
+                             method = "rFerns", 
+                             trControl = cctrl1,
+                             preProc = c("center", "scale"),
+                             ferns = 50)
+
+test_class_pred <- predict(test_class_cv_model, testing[, -ncol(testing)])
+
+set.seed(849)
+test_class_loo_model <- train(trainX, trainY, 
+                              method = "rFerns", 
+                              trControl = cctrl2,
+                              preProc = c("center", "scale"),
+                              ferns = 50)
+test_levels <- levels(test_class_cv_model)
+
+#########################################################################
+
+sInfo <- sessionInfo()
+
+save(list = c(tests, "sInfo", "timestamp"),
+     file = file.path(getwd(), paste(model, ".RData", sep = "")))
+
+q("no")
+
