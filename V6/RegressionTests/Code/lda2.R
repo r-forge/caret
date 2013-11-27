d3017e2c16d5a99282bf057db827f148828d9f7c
kuhn (Dr. ) <mxkuhn@gmail.com>
2013-11-27 16:02:29 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@680 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/RegressionTests/Code/lda2.R
@@ -0,0 +1,56 @@
+library(caret)
+timestamp <- format(Sys.time(), "%Y_%m_%d_%H_%M")
+
+model <- modelInfo
+tests <- c("test_class_cv_model", "test_class_pred", "test_class_prob",
+           "test_class_loo_model", "test_levels")
+
+#########################################################################
+
+set.seed(1)
+
+library(caret)
+
+library(mlbench)
+data(Glass)
+Glass$Type <- factor(paste("Class", as.numeric(Glass$Type), sep = ""))
+
+set.seed(2)
+
+inTrain <- createDataPartition(Glass$Type, p = .90)
+trainX <-Glass[inTrain[[1]], -ncol(Glass)]
+trainY <-Glass$Type[inTrain[[1]]]
+testX <-Glass[-inTrain[[1]], -ncol(Glass)]
+testY <- Glass$Type[-inTrain[[1]]]
+
+cctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all",
+                       classProbs = TRUE)
+cctrl2 <- trainControl(method = "LOOCV",
+                       classProbs = TRUE)
+
+set.seed(849)
+test_class_cv_model <- train(trainX, trainY, 
+                             method = modelInfo, 
+                             trControl = cctrl1,
+                             preProc = c("center", "scale"))
+
+test_class_pred <- predict(test_class_cv_model, testX)
+test_class_prob <- predict(test_class_cv_model, testX, type = "prob")
+
+set.seed(849)
+test_class_loo_model <- train(trainX, trainY, 
+                            method = modelInfo, 
+                            trControl = cctrl2,
+                            preProc = c("center", "scale"))
+test_levels <- levels(test_class_cv_model)
+
+#########################################################################
+
+sInfo <- sessionInfo()
+
+save(list = c(tests, "sInfo", "timestamp"),
+     file = file.path(getwd(), paste(model, ".RData", sep = "")))
+
+q("no")
+
+
