1d6fc7cdff2baf69feb661db44bdbf260f0474e0
kuhn (Dr. ) <mxkuhn@gmail.com>
2013-11-28 03:29:51 +0000

    initial verison
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@686 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/caret/inst/models/penalized.R
@@ -0,0 +1,26 @@
+modelInfo <- list(library = "penalized",
+                  type = c('Regression'),
+                  parameters = data.frame(parameter = c('lambda1', 'lambda2'),
+                                          class = c('numeric', 'numeric'),
+                                          label = c('L1 Penalty', 'L2 Penalty')),
+                  grid = function(x, y, len = NULL) 
+                    expand.grid(.lambda1 = 2^((1:len) -1),
+                                .lambda2 = 2^((1:len) -1)),
+                  loop = NULL,
+                  fit = function(x, y, wts, param, lev, last, classProbs, ...) {
+                    penalized(y, x,
+                              model = "linear",
+                              lambda1 = param$.lambda1,
+                              lambda2 = param$.lambda2,
+                              ...) 
+                  },
+                  predict = function(modelFit, newdata, submodels = NULL) {
+                    out <- predict(modelFit, newdata)
+                    out <- if(is.vector(out)) out["mu"] else out[,"mu"]
+                    out
+                  },
+                  prob = NULL,
+                  tags = c("Implicit Feature Selection", 
+                           "L1 Regularization", "L2 Regularization",
+                           "Linear Regression"),
+                  sort = function(x) x)
