32ef948cbf5ed5e83a746c9a2a2fd999a479f104
kuhn (Dr. ) <mxkuhn@gmail.com>
2013-11-28 03:28:54 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@685 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ V6/RegressionTests/Code/logicBag.R
@@ -0,0 +1,82 @@
+library(caret)
+timestamp <- format(Sys.time(), "%Y_%m_%d_%H_%M")
+
+model <- "logicBag"
+
+#########################################################################
+gobinary <- function(x){
+  out <- apply(x, 2, function(x) ifelse(x > mean(x), 1, 0))
+  colnames(out) <- colnames(x)
+  out
+} 
+
+set.seed(2)
+training <- twoClassSim(100)
+testing <- twoClassSim(500)
+trainX <- gobinary(training[, -ncol(training)])
+trainY <- training$Class
+testX <- gobinary(testing[, -ncol(testing)])
+testY <- testing$Class
+
+cctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all")
+cctrl2 <- trainControl(method = "LOOCV")
+
+set.seed(849)
+test_class_cv_model <- train(trainX, trainY, 
+                             method = "logicBag", 
+                             trControl = cctrl1,
+                             B = 3)
+
+test_class_pred <- predict(test_class_cv_model, testX)
+
+set.seed(849)
+test_class_loo_model <- train(trainX, trainY, 
+                              method = "logicBag", 
+                              trControl = cctrl2,
+                              B = 3)
+test_levels <- levels(test_class_cv_model)
+
+#########################################################################
+
+data(BloodBrain)
+bbbDescr <-bbbDescr[, -nearZeroVar(bbbDescr)]
+bbbDescr <-bbbDescr[, -findCorrelation(cor(bbbDescr), .5)]
+numVal <- apply(bbbDescr, 2, function(x) length(unique(x)))
+bbbDescr <- bbbDescr[, numVal > 10]
+
+set.seed(2)
+
+inTrain <- createDataPartition(logBBB, p = .5)
+trainX <-gobinary(bbbDescr[inTrain[[1]], ])
+trainY <- logBBB[inTrain[[1]]]
+testX <- gobinary(bbbDescr[-inTrain[[1]], ])
+testY <- logBBB[-inTrain[[1]]]
+
+rctrl1 <- trainControl(method = "cv", number = 3, returnResamp = "all")
+rctrl2 <- trainControl(method = "LOOCV")
+
+set.seed(849)
+test_reg_cv_model <- train(trainX, trainY, 
+                           method = "logicBag", 
+                           trControl = rctrl1,
+                           B = 3)
+test_reg_pred <- predict(test_reg_cv_model, testX)
+
+set.seed(849)
+test_reg_loo_model <- train(trainX, trainY, 
+                            method = "logicBag",
+                            trControl = rctrl2,
+                            B = 3)
+
+#########################################################################
+
+tests <- grep("test_", ls(), fixed = TRUE, value = TRUE)
+
+sInfo <- sessionInfo()
+
+save(list = c(tests, "sInfo", "timestamp"),
+     file = file.path(getwd(), paste(model, ".RData", sep = "")))
+
+q("no")
+
+
