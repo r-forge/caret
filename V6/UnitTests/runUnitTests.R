1bc87ae268cac5cce72ed5700455cc0f9845278f
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-29 21:29:32 +0000

    Checking more elements of the models.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@710 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- V6/UnitTests/runUnitTests.R
+++ V6/UnitTests/runUnitTests.R
@@ -1,7 +1,7 @@
 suppressMessages(library(caret))
 
 assert <- function(assertion, ..., fail=FALSE, file=stderr()) {
-  s <- deparse(substitute(assertion))
+  s <- deparse(substitute(assertion), width.cutoff=500L)
   if (! assertion) {
     if (length(list(...)) > 0) {
       msg <- sprintf('assertion failure: %s: %s', s, sprintf(...))
@@ -20,10 +20,13 @@ assert <- function(assertion, ..., fail=FALSE, file=stderr()) {
 }
 
 runUnitTests <- function(x, y, f) {
+  legalTypes <- c('Classification', 'Regression')
+  loopFormals <- c('grid')
   gridFormals <- c('x', 'y', 'len')
   fitFormals <- c('x', 'y', 'wts', 'param', 'lev', 'last', 'classProbs', '...')
   predictFormals <- c('modelFit', 'newdata', 'submodels')
   probFormals <- c('modelFit', 'newdata', 'submodels')
+  # varImpFormals <- c('object', 'estimate', '...')
   sortFormals <- c('x')
 
   models <- getModelInfo()
@@ -43,28 +46,60 @@ runUnitTests <- function(x, y, f) {
                'unable to load package %s', m$library[i], file=files)
       }
 
+      assert(is.character(m$type) && length(m$type) %in% c(1, 2) &&
+             all(m$type %in% legalTypes),
+             "type is set incorrectly",
+             file=files)
+      assert(is.null(m$loop) || is.function(m$loop),
+             "loop isn't NULL or a function",
+             file=files)
+      assert(! is.function(m$loop) || all(names(formals(m$loop)) == loopFormals),
+             "loop has incorrect arguments",
+             file=files)
       assert(is.data.frame(m$parameters),
-             "parameters isn't a data frame", file=files)
+             "parameters isn't a data frame",
+             file=files)
       assert(is.function(m$grid),
-             "grid isn't a function", file=files)
+             "grid isn't a function",
+             file=files)
       assert(! is.function(m$grid) || all(names(formals(m$grid)) == gridFormals),
-             "grid has incorrect arguments", file=files)
+             "grid has incorrect arguments",
+             file=files)
       assert(is.function(m$fit),
-             "fit isn't a function", file=files)
+             "fit isn't a function",
+             file=files)
       assert(! is.function(m$fit) || all(names(formals(m$fit)) == fitFormals),
-             "fit has incorrect arguments", file=files)
+             "fit has incorrect arguments",
+             file=files)
       assert(is.function(m$predict),
-             "predict isn't a function", file=files)
+             "predict isn't a function",
+             file=files)
       assert(! is.function(m$predict) || all(names(formals(m$predict)) == predictFormals),
-             "predict has incorrect arguments", file=files)
-      assert(is.function(m$prob) || is.null(m$prob),
-             "prob isn't a function", file=files)
+             "predict has incorrect arguments",
+             file=files)
+      assert(is.null(m$prob) || is.function(m$prob),
+             "prob isn't NULL or a function",
+             file=files)
       assert(! is.function(m$prob) || all(names(formals(m$prob)) == probFormals),
-             "prob has incorrect arguments", file=files)
+             "prob has incorrect arguments",
+             file=files)
+      assert(is.null(m$varImp) || is.function(m$varImp),
+             "varImp isn't NULL or a function",
+             file=files)
+      fargs <- names(formals(m$varImp))
+      fargc <- length(fargs)
+      assert(! is.function(m$varImp) || (fargs[1] == 'object' && fargs[fargc] == '...'),
+             "varImp has incorrect arguments",
+             file=files)
+      assert(is.character(m$tag),
+             "tag is not character type",
+             file=files)
       assert(is.function(m$sort),
-             "sort isn't a function", file=files)
+             "sort isn't a function",
+             file=files)
       assert(! is.function(m$sort) || all(names(formals(m$sort)) == sortFormals),
-             "sort has incorrect arguments", file=files)
+             "sort has incorrect arguments",
+             file=files)
     },
     error=function(e) {
       cat(paste0('error: ', conditionMessage(e), '\n'), file=stderr())
