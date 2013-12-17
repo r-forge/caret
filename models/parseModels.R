84ab2e9e2a8dadfbe1a43c1f05b823be1d018472
kuhn <kuhn@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-17 17:30:36 +0000

    initial version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@751 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ models/parseModels.R
@@ -0,0 +1,16 @@
+setwd("~/Code/caret/V6//caret/inst/")
+modelFiles <- list.files(path = "models", pattern = "\\.R$")
+
+models <- vector(mode = "list", length = length(modelFiles))
+names(models) <- gsub("\\.R", "", modelFiles)
+
+for(i in seq(along = modelFiles)) {
+  source(file.path("models", modelFiles[i]))
+  models[[i]] <- modelInfo
+  rm(modelInfo)
+}
+
+save(models, file = file.path("models", "models.RData"))
+
+cat("\nYou can update your caret installation using the command:\n\n")
+cat(sprintf("  cp models.RData %s/.\n", system.file("models", package="caret")))
