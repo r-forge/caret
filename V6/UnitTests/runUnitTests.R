560081dee44a4fc1c9a6dae0a8ca333fbe324f8c
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-30 20:04:04 +0000

    Added traceback info in the log file for errors.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/caret@802 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- V6/UnitTests/runUnitTests.R
+++ V6/UnitTests/runUnitTests.R
@@ -46,110 +46,128 @@ local({
       log('\nTesting %s...', modelName, files=files)
       m <- models[[i]]
       tryCatch({
-        for(i in seq(along = m$library)) {
-          assert(require(m$library[i], character.only=TRUE, quietly=TRUE),
-                 'unable to load package %s', m$library[i], files=files)
-        }
-
-        assert(is.character(m$type) && length(m$type) %in% c(1, 2) &&
-               all(m$type %in% legalTypes),
-               "type is set incorrectly",
-               files=files)
-        assert(is.null(m$loop) || is.function(m$loop),
-               "loop isn't NULL or a function",
-               files=files)
-        assert(! is.function(m$loop) || all(names(formals(m$loop)) == loopFormals),
-               "loop has incorrect arguments",
-               files=files)
-        assert(is.data.frame(m$parameters),
-               "parameters isn't a data frame",
-               files=files)
-        assert(is.function(m$grid),
-               "grid isn't a function",
-               files=files)
-        assert(! is.function(m$grid) || all(names(formals(m$grid)) == gridFormals),
-               "grid has incorrect arguments",
-               files=files)
-        assert(is.function(m$fit),
-               "fit isn't a function",
-               files=files)
-        assert(! is.function(m$fit) || all(names(formals(m$fit)) == fitFormals),
-               "fit has incorrect arguments",
-               files=files)
-        assert(is.function(m$predict),
-               "predict isn't a function",
-               files=files)
-        assert(! is.function(m$predict) ||
-               all(names(formals(m$predict)) == predictFormals),
-               "predict has incorrect arguments",
-               files=files)
-        assert(is.null(m$prob) || is.function(m$prob),
-               "prob isn't NULL or a function",
-               files=files)
-        assert(! is.function(m$prob) || all(names(formals(m$prob)) == probFormals),
-               "prob has incorrect arguments",
-               files=files)
-        assert(is.null(m$varImp) || is.function(m$varImp),
-               "varImp isn't NULL or a function",
-               files=files)
-        fargs <- names(formals(m$varImp))
-        fargc <- length(fargs)
-        assert(! is.function(m$varImp) ||
-               (fargs[1] == 'object' && fargs[fargc] == '...'),
-               "varImp has incorrect arguments",
-               files=files)
-        assert(is.character(m$tag),
-               "tag is not character type",
-               files=files)
-        assert(is.function(m$sort),
-               "sort isn't a function",
-               files=files)
-        assert(! is.function(m$sort) || all(names(formals(m$sort)) == sortFormals),
-               "sort has incorrect arguments",
-               files=files)
-
-        tuneGrid <- m$grid(x, y, 3)
-        assert(is.data.frame(tuneGrid))
-
-        if ("Classification" %in% m$type) {
-          wts <- rep(1L, nrow(x))  # also try NULL?
-          tuneValue <- tuneGrid[1,,drop=FALSE]
-          obsLevels <- levels(y)
-          log("Calling fit function for %s", modelName, files=files)
-          modelFit <- m$fit(x, y, wts=wts, param=tuneValue,
-                         lev=obsLevels, last=FALSE,
-                         classProbs=is.function(m$prob))
-          print(class(modelFit))
-          assert(! is.null(modelFit),
-                 "fit function returned a NULL",
-                 fail=TRUE,
-                 files=files)
-
-          if (!isS4(modelFit)) {
-            modelFit$vNames <- colnames(x)
-            modelFit$problemType <- "Classification"
-            modelFit$tuneValue <- tuneValue
-            modelFit$obsLevels <- obsLevels
+        withCallingHandlers({
+          for(i in seq(along = m$library)) {
+            assert(require(m$library[i], character.only=TRUE, quietly=TRUE),
+                   'unable to load package %s', m$library[i], files=files)
           }
 
-          log("Calling predict function for %s", modelName, files=files)
-          if (interactive())
-            debug(m$predict)
-          xp <- m$predict(modelFit, x)
-          assert(! is.null(xp),
-                 "predict function returned a NULL",
+          assert(is.character(m$type) && length(m$type) %in% c(1, 2) &&
+                 all(m$type %in% legalTypes),
+                 "type is set incorrectly",
+                 files=files)
+          assert(is.null(m$loop) || is.function(m$loop),
+                 "loop isn't NULL or a function",
+                 files=files)
+          assert(! is.function(m$loop) || all(names(formals(m$loop)) == loopFormals),
+                 "loop has incorrect arguments",
+                 files=files)
+          assert(is.data.frame(m$parameters),
+                 "parameters isn't a data frame",
+                 files=files)
+          assert(is.function(m$grid),
+                 "grid isn't a function",
+                 files=files)
+          assert(! is.function(m$grid) || all(names(formals(m$grid)) == gridFormals),
+                 "grid has incorrect arguments",
+                 files=files)
+          assert(is.function(m$fit),
+                 "fit isn't a function",
+                 files=files)
+          assert(! is.function(m$fit) || all(names(formals(m$fit)) == fitFormals),
+                 "fit has incorrect arguments",
+                 files=files)
+          assert(is.function(m$predict),
+                 "predict isn't a function",
+                 files=files)
+          assert(! is.function(m$predict) ||
+                 all(names(formals(m$predict)) == predictFormals),
+                 "predict has incorrect arguments",
+                 files=files)
+          assert(is.null(m$prob) || is.function(m$prob),
+                 "prob isn't NULL or a function",
+                 files=files)
+          assert(! is.function(m$prob) || all(names(formals(m$prob)) == probFormals),
+                 "prob has incorrect arguments",
+                 files=files)
+          assert(is.null(m$varImp) || is.function(m$varImp),
+                 "varImp isn't NULL or a function",
+                 files=files)
+          fargs <- names(formals(m$varImp))
+          fargc <- length(fargs)
+          assert(! is.function(m$varImp) ||
+                 (fargs[1] == 'object' && fargs[fargc] == '...'),
+                 "varImp has incorrect arguments",
+                 files=files)
+          assert(is.character(m$tag),
+                 "tag is not character type",
+                 files=files)
+          assert(is.function(m$sort),
+                 "sort isn't a function",
+                 files=files)
+          assert(! is.function(m$sort) || all(names(formals(m$sort)) == sortFormals),
+                 "sort has incorrect arguments",
                  files=files)
 
-          if (is.function(m$prob)) {
-            log("Calling prob function for %s", modelName, files=files)
-            p <- m$prob(modelFit, x)
-            assert(! is.null(p),
-                   "prob function returned a NULL",
+          tuneGrid <- m$grid(x, y, 3)
+          assert(is.data.frame(tuneGrid))
+
+          if ("Classification" %in% m$type) {
+            wts <- rep(1L, nrow(x))  # also try NULL?
+            tuneValue <- tuneGrid[1,,drop=FALSE]
+            obsLevels <- levels(y)
+            log("Calling fit function for %s", modelName, files=files)
+            modelFit <- m$fit(x, y, wts=wts, param=tuneValue,
+                           lev=obsLevels, last=FALSE,
+                           classProbs=is.function(m$prob))
+            print(class(modelFit))
+            assert(! is.null(modelFit),
+                   "fit function returned a NULL",
+                   fail=TRUE,
                    files=files)
+
+            if (!isS4(modelFit)) {
+              modelFit$vNames <- colnames(x)
+              modelFit$problemType <- "Classification"
+              modelFit$tuneValue <- tuneValue
+              modelFit$obsLevels <- obsLevels
+            }
+
+            log("Calling predict function for %s", modelName, files=files)
+            if (interactive())
+              debug(m$predict)
+            xp <- m$predict(modelFit, x)
+            assert(! is.null(xp),
+                   "predict function returned a NULL",
+                   files=files)
+
+            if (is.function(m$prob)) {
+              log("Calling prob function for %s", modelName, files=files)
+              p <- m$prob(modelFit, x)
+              assert(! is.null(p),
+                     "prob function returned a NULL",
+                     files=files)
+            }
           }
-        }
+        },
+        error=function(e) {
+          e$calls <- sys.calls()
+          signalCondition(e)
+        })
       },
       error=function(e) {
+        if (length(e$calls) > 0) {
+          cat('traceback (most recent call first):\n', file=logfile)
+          calls <- rev(e$calls)[c(-1, -2)]
+          for (xcall in calls) {
+            if (identical(xcall[[1]], as.name('withCallingHandlers')))
+              break
+            cat('> ', file=logfile)
+            sink(logfile)
+            print(xcall)
+            sink()
+          }
+        }
         cat(paste0('error: ', conditionMessage(e), '\n'), file=stderr())
         cat(sprintf('error testing model %s: continuing to next model\n', modelName))
         cat(sprintf('error: %s: %s\n', modelName, conditionMessage(e)), file=logfile)
